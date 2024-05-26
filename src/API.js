55254326e4e94304feae886671f30b4194e44bd9
Vu Quoc Hung <92137335+hungnongtrai2204@users.noreply.github.com>
2024-05-26 20:25:02 +0700

    Update API.js

--- src/API.js
+++ src/API.js
@@ -1,11 +1,11 @@
-// export const API_MOVIE = 'http://localhost:2000/api';
-// export const API_CINEMA_SYSTEM = 'http://localhost:8001';
-// export const API_SHOWTIME = 'http://localhost:2002/api';
-// export const API_TICKET = 'http://localhost:2003/api';
-// export const API_USER = 'http://localhost:8000';
+export const API_MOVIE = 'http://localhost:2000/api';
+export const API_CINEMA_SYSTEM = 'http://localhost:8001';
+export const API_SHOWTIME = 'http://localhost:2002/api';
+export const API_TICKET = 'http://localhost:2003/api';
+export const API_USER = 'http://localhost:8000';
 
-export const API_MOVIE = 'https://movie-api-vpbe.onrender.com/api';
-export const API_CINEMA_SYSTEM = 'https://cinema-api-61nr.onrender.com';
-export const API_SHOWTIME = 'https://showtime-api-k9kq.onrender.com/api';
-export const API_TICKET = 'https://ticket-api-a67j.onrender.com/api';
-export const API_USER = 'https://user-api-btn4.onrender.com';
+// export const API_MOVIE = 'https://movie-api-vpbe.onrender.com/api';
+// export const API_CINEMA_SYSTEM = 'https://cinema-api-61nr.onrender.com';
+// export const API_SHOWTIME = 'https://showtime-api-k9kq.onrender.com/api';
+// export const API_TICKET = 'https://ticket-api-a67j.onrender.com/api';
+// export const API_USER = 'https://user-api-btn4.onrender.com';
